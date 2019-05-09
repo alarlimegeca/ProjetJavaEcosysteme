c5964db334a50eaffb51453b2eb71f590a0d7d83
LouiseGrall <49550835+LouiseGrall@users.noreply.github.com>
2019-05-09 10:40:15 +0200

    Add files via upload

---
+++ Carnivore.java
@@ -0,0 +1,98 @@
+package animaux;
+import java.awt.Color;
+/**
+ * 
+ * @author formation
+ *
+ */
+import java.util.ArrayList;
+
+import ecosysteme.Case;
+import ecosysteme.Grille;
+
+/**
+ * Classe décrivant les carnivores
+ * @author Paul,Armand et Louise 
+ *
+ */
+public abstract class Carnivore extends Animal{
+	/**
+	 * Constructeur
+	 * @param id
+	 * @param dateNaissance
+	 * @param dateDeces
+	 * @param accesForet
+	 * @param esperanceVie
+	 * @param vitesse
+	 * @param vivant
+	 * @param tpDecomposition
+	 * @param espece
+	 * @param tailleEstomac
+	 * @param remplissageEstomac
+	 * @param viande
+	 * @param maturite
+	 * @param aProcree
+	 * @param meurtFaim
+	 */
+	public Carnivore(int dateNaissance, Case emplacement, 
+			int tpDecomposition, Color couleur, int remplissageEstomac,  int maturite,
+			boolean aProcree,int meurtFaim) {
+		super(dateNaissance,emplacement, tpDecomposition,couleur,
+				remplissageEstomac, maturite,aProcree,meurtFaim);
+	}
+
+	public void seNourrir(){
+		if (getEstVivant() == false) {
+		} else {
+			if (this.getRemplissageEstomac() < this.getTailleEstomac()) {
+				//		Création de la liste des cases adjacentes
+				ArrayList<Case> cases = new ArrayList<Case>();
+				//			Définition des cases adjacentes
+				Case case1 = Grille.getCase(this.getEmplacement().getX()-1,this.getEmplacement().getY()-1);	// marche avec les coordonn�es de la case
+				cases.add(case1);
+				Case case2 = Grille.getCase(this.getEmplacement().getX(),this.getEmplacement().getY()-1);
+				cases.add(case2);
+				Case case3 = Grille.getCase(this.getEmplacement().getX()+1,this.getEmplacement().getY()-1);
+				cases.add(case3);
+				Case case4 = Grille.getCase(this.getEmplacement().getX()-1,this.getEmplacement().getY());
+				cases.add(case4);
+				Case case5 = Grille.getCase(this.getEmplacement().getX()+1,this.getEmplacement().getY());
+				cases.add(case5);
+				Case case6 = Grille.getCase(this.getEmplacement().getX()-1,this.getEmplacement().getY()+1);
+				cases.add(case6);
+				Case case7 = Grille.getCase(this.getEmplacement().getX(),this.getEmplacement().getY()+1);
+				cases.add(case7);
+				Case case8 = Grille.getCase(this.getEmplacement().getX()+1,this.getEmplacement().getY()+1);
+				cases.add(case8);
+				for (Case c : cases) {
+					// si la case contient un animal, et que l'animal n'est pas un autre carnivore, 
+					// le carnivore le tue
+					if(c.getEstVide() == true || c.getAnimal().getEspece() != this.getEspece()) {
+						c.getAnimal().decede();
+						if (getRemplissageEstomac() < getTailleEstomac()) {
+							// si l'animal tué contient plus de nourriture que l'animal n'a de place dans son estomac, 
+							// alors il mange juste à sa faim
+							if (c.getAnimal().getViande() > (this.getTailleEstomac() - this.getRemplissageEstomac())) {
+								c.getAnimal().setViande(c.getAnimal().getViande() - (this.getTailleEstomac() - this.getRemplissageEstomac()));
+								this.setRemplissageEstomac(this.getTailleEstomac());
+								this.setRemplissageEstomac(getRemplissageEstomac() + c.getAnimal().getViande());
+							} 
+							// sinon si l'animal a suffisament faim et que la case ne contient pas suffisament ou juste assez
+							// de nourriture pour le rassasier, il mange tout la nourriture présente sur la case
+							// le stock de nourriture tombe donc à 0
+							else if (getRemplissageEstomac() + c.getAnimal().getViande() <= getTailleEstomac()){
+								this.setRemplissageEstomac(getRemplissageEstomac() + c.getAnimal().getViande());
+								c.getAnimal().setViande(this.getTailleEstomac() - this.getRemplissageEstomac());
+								c.getAnimal().setViande(0);
+								c.setACadavre(false); 
+							}
+						}
+					}			
+					break;			//une fois que l'animal s'est nourri on arrête la boucle (un animal ne se nourrit qu'une seule fois par tour
+				}
+
+			}
+		}
+	}
+	public abstract void seReproduire();
+}
---
+++ Crocodile.java
@@ -0,0 +1,129 @@
+package animaux;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import Gestion.Gestionnaire;
+import ecosysteme.Case;
+import ecosysteme.Grille;
+
+/**
+ * Classe décrivant les crocodiles
+ * @author Paul,Armand et Louise 
+ *
+ */
+
+public class Crocodile extends Carnivore{
+	/**
+	 * Constructeur
+	 * @param dateNaissance
+	 * @param dateDeces
+	 * @param accesForet
+	 * @param esperanceVie
+	 * @param vitesse
+	 * @param estVivant
+	 * @param tpDecomposition
+	 * @param espece
+	 * @param tailleEstomac
+	 * @param remplissageEstomac
+	 * @param viande
+	 * @param maturite
+	 * @param aProcree
+	 * @param meurtFaim
+	 */
+	public Crocodile(int dateNaissance,  Case emplacement,
+			int tpDecomposition,  Color couleur, int remplissageEstomac,  int maturite,
+			boolean aProcree,int meurtFaim) {
+		super(dateNaissance, emplacement, tpDecomposition,couleur,
+				remplissageEstomac,maturite,aProcree,meurtFaim);
+
+		// on donne un id à l'animal
+		this.id = Gestionnaire.getAnimaux().size() + 1;
+
+		//		donne une espérance de vie d'au moins 70 tours et pouvant aller jusqu'à 1/5 de plus
+		this.esperanceVie = 70;
+		esperanceVie = esperanceVie + (int)(Math.random() * this.esperanceVie/5);
+
+		// le crocodile a accès aux cases de forêt
+		accesForet = true;
+
+		// le crocodile a une vitesse de 1
+		vitesse = 1;
+	}
+
+	/**
+	 * 	Cette méthode permet à 2 animaux de la même espèce de se reproduire s'ils sont sur des cases adjacentes et de produire ainsi 
+	 *  un nouvel animal de la même espèce.
+	 */
+	public void seReproduire() {
+		if (this.getEstVivant() == false) {					// on vérifie que l'animal est vivant (on ne se reproduit que vivant, c'est la règle...)
+		} else {
+			if (this.getAProcree() == true) {				// on vérifie que l'animal est mature et ne s'est pas reproduit récemment
+			} else {
+
+				// il faut mettre la reproduction dans les classes des animaux 
+				// car Animal est une classe abstraite (on ne peut pas instancier un objet d'une classe abstraite
+				// Création de la liste des cases adjacentes
+				ArrayList<Case> cases = new ArrayList<Case>();
+				// Définition des cases adjacentes
+				Case case1 = Grille.getCase(this.getEmplacement().getX()-1, this.getEmplacement().getY()-1);
+				cases.add(case1);
+				Case case2 = Grille.getCase(this.getEmplacement().getX(), this.getEmplacement().getY()-1);
+				cases.add(case2);
+				Case case3 = Grille.getCase(this.getEmplacement().getX()+1, this.getEmplacement().getY()-1);
+				cases.add(case3);
+				Case case4 = Grille.getCase(this.getEmplacement().getX()-1, this.getEmplacement().getY());
+				cases.add(case4);
+				Case case5 = Grille.getCase(this.getEmplacement().getX()+1, this.getEmplacement().getY());
+				cases.add(case5);
+				Case case6 = Grille.getCase(this.getEmplacement().getX()-1, this.getEmplacement().getY()+1);
+				cases.add(case6);
+				Case case7 = Grille.getCase(this.getEmplacement().getX(), this.getEmplacement().getY()+1);
+				cases.add(case7);
+				Case case8 = Grille.getCase(this.getEmplacement().getX()+1, this.getEmplacement().getY()+1);
+				cases.add(case8);
+				for (Case c : cases){ 
+					if (c.getEstVide() == false) {											//			Si une des cases adjacentes n'est pas vide, 
+					}																		
+					if (c.getAnimal().getEstVivant() == true )								// si l'animal présent sur la case adjacente est vivant
+						if (c.getAnimal().getEspece().equals(this.getEspece()))	{			// si l'animal présent sur la case adjacente est de la même espèce, 
+							if (c.getAnimal().getAProcree() == false) {						// et enfin si l'autre animal n'a pas déjà procréé récemment 
+								this.setAProcree(true);											// 			la variable permettant de savoir si l'animal a procree devient true
+								for (Case cbis : cases){											// 			on cherche ensuite  
+									if (c.getEstVide() == true) {									//			une case vide 
+										Animal crocodile = new Crocodile (Gestionnaire.getTour(),cbis,
+												this.getTempsDecomposition(),  this.couleur, this.tailleEstomac/2,  this.getMaturite(),	// pour créer un nouvel individu
+												this.getAProcree(),this.getMeurtFaim());
+										Gestionnaire.addAnimal(crocodile);								//			on ajoute l'animal au gestionnaire
+										break;														//			l'animal se reproduit et arrête de vérifier 
+									}
+								}
+							}
+						}
+				}
+			}
+		}
+	}
+
+
+	public void croissance() {
+		/*
+		 * définition de la taille de l'estomac et de la viande disponible sur l'animal en fonction de son âge et de 
+		 * son espèce.
+		 * cette fonction est activée par le Gestionnaire en début de tour
+		 */
+		if ((Gestionnaire.getTour()-getDateNaissance())<=(esperanceVie/4)) {
+			setViande(2);
+			setTailleEstomac(2);
+		}
+		else if((Gestionnaire.getTour()-getDateNaissance())<=(esperanceVie/2)) {
+			setViande(5);
+			setTailleEstomac(4);
+		}
+		else {
+			setTailleEstomac(8);
+			setViande(10);
+		}
+	}
+
+}
